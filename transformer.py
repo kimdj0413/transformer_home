566a890efa4a19d4d6df8433b2d185a59bc5e589
kindj0413 <ejuniya@naver.com>
2024-07-02 16:09:39 +0900

    202400702

---
+++ transformer.py
@@ -0,0 +1,10 @@
+import pickle
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import urllib.request
+from konlpy.tag import Okt
+from tqdm import tqdm
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
